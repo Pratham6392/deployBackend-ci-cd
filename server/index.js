f95626e2afa7403140e2f4f464b989c18938752b
Pratham khatri <khatripratham349@gmail.com>
2024-04-11 23:30:25 +0530

    addddddddd

---
+++ server/index.js
@@ -0,0 +1,25 @@
+
+const mongoose = require('mongoose');
+const cors = require('cors');
+const adminRouter = require("./routes/admin");
+const userRouter = require("./routes/user");
+const express = require('express');
+
+// 
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/admin", adminRouter)
+app.use("/user", userRouter)
+
+
+// Connect to MongoDB
+// DONT MISUSE THIS THANKYOU!!
+mongoose.connect('mongodb://localhost:27017/courses', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
+
+app.listen(3000, () => console.log('Server running on port 3000'));
+
+
+
